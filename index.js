f08f284352bfc7f03531eeb8e9ff8dd119a0d6e7
ondichokev <ondichokev@gmail.com>
2024-08-01 13:07:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42;
+    return Math.abs(someValue - hqLocation);
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(destination - start);
+    return blocksTravelled * 264;
+  }
+  
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);}
+    
+    if (distanceInFeet <= 400) {
+      return 0; 
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02; 
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25; 
+    } else {
+      return 'cannot travel that far'; 
+  }
+  
